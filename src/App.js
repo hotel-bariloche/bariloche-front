76ef27a46de58f62f1299145e0b2dae4645cdd9e
mireiaGomariz <mireiapgomariz@gmail.com>
2020-06-11 08:40:08 +0200

    local changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <Navbar/>
+        <Switch>
+          <Route path="/"/>
+          <Route path="/habitaciones"/>
+          <Route path="/blog"/>
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
