d9db58c5f636943be08718c1743bc251f70409ca
AlundraOz <ailuozols@gmail.com>
2020-06-11 19:37:27 +0200

    added welcome page

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import '../css/style.css'
-import Sunset from './img/gallery/sunset.png'
+import '../css/style.css';
 
 const Banner = () => {
   return(
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Banner from './Banner'
+import Banner from './Banner';
+import AboutUs from './AboutUs'
 
 
 const Home = () => {
     return(
         <div>
             <Banner/>
+            <AboutUs/>
         </div>
     )
 }
