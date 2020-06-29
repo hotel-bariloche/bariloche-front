868b5304bffdd8342584e42f42f2e223907bba01
mireiaGomariz <mireiapgomariz@gmail.com>
2020-06-29 07:59:30 +0200

    quit explore rooms in the home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,6 @@ import BannerHome from './BannerHome';
 import AboutUs from './AboutUs';
 import DestinoBariloche from './DestinoBariloche';
 import Video from './Video';
-import ExploreRooms from '../exploreRooms';
 import Reviews from './Reviews';
 import ContactHome from './Contact.js';
 
@@ -16,7 +15,6 @@ const Home = () => {
             <AboutUs/>
             <DestinoBariloche/>
             <Video/>
-            <ExploreRooms/>
             <Reviews/>
             <ContactHome/>
         </div>
