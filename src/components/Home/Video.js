fa6b556d6529a90818c112613b2fa152513a0de6
AlundraOz <ailuozols@gmail.com>
2020-06-30 12:20:30 +0200

    video section modal done, ask for texts

--- src/components/Home/Video.js
+++ src/components/Home/Video.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 const Video = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
     return(
-        <section className="video-area">
-        <div className="container">
-          <div className="row justify-content-center align-items-center flex-column text-center">
-            <a id="play-home-video" className="video-play-button" href="https://www.youtube.com/watch?v=WU_dWnyGZSo">
-              <span></span>
-            </a>
-            <h3>Seaplace</h3>
-            <p>View four has said does men saw find dear shy talent</p>
-          </div>
-        </div>  
-      </section>
+      <div>
+          <section className="video-area">
+            <div className="container">
+              <div className="row justify-content-center align-items-center flex-column text-center">
+                <a onClick={handleShow} id="play-home-video" className="video-play-button">
+                  <span></span>
+                </a>
+                <h3>Seaplace</h3>
+                <p>View four has said does men saw find dear shy talent</p>
+              </div>
+            </div>  
+          </section>
+
+          <Modal  size="lg" show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Modal heading</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <iframe 
+                  width="760" height="400" src="https://www.youtube.com/embed/WU_dWnyGZSo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+              </iframe>
+          </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
     )
 }
 
 export default Video
 
-/*<iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLx0sYbCqOb8TBPRdmBHs5Iftvv9TPboYG" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>*/
+/*<iframe width="560" height="315" src="https://www.youtube.com/watch?v=WU_dWnyGZSo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>*/
