27ce7b3c7ac96d719cd7b27cb7ae5378f84e5cf3
mireiaGomariz <mireiapgomariz@gmail.com>
2020-06-30 17:56:28 +0200

    fix curly

--- src/Utils/Texts.js
+++ src/Utils/Texts.js
@@ -19,130 +19,131 @@ export const Texts = {
                 hotel: 'La inolvidable experiencia de disfrutar Bariloche más que nunca. A metros del Lago Nahuel Huapi, se encuentra nuestro hotel, con 75 habitaciones listas para recibirte. Está pensado para aquellos visitantes que quieren venir a disfrutar de nuestro destino, pero a la vez poder relajarse en un lugar tranquilo, y así, poder seguir planeando la travesía. Nuestros clientes son nuestros amigos e invitados, por lo tanto , estamos preparados para hacerte sentir en casa!',
                 place: 'Bariloche, Capital Nacional del Turismo Aventura y del Chocolate, es uno de los destinos más buscados dentro de Argentina. ¿Por que? Está rodeada de bosques milenarios, montañas imponentes blanquesinas y lagos sacados de cuentos. Podes disfrutar un clima cálido en primavera- verano,  con días mas largos para aprovecharlos al máximo haciendo senderismo al aire libre, visitando las playas lacustres, o realizar excursiones a las reservas naturales. En invierno podrás disfrutar de la nieve, de paisajes soñados y si te gustan los deportes como el sky o snowboard, esta es tu estación para visitarnos.',
             },
-        video: 'titulo del video',
-        guests: {
-            title: 'Nuestros clientes nos quieren',
+            video: 'titulo del video',
+            guests: {
+                title: 'Nuestros clientes nos quieren',
+                cards: {
+                    first: {
+                        review: 'Estuvimos 3 noches alojados en pareja la pasamos muy bien. La atención del personal del comedor excelente. Habitaciones bien calefaccionadas, la y desayuno muy bueno y variado. Lo recomiendo en un 100%',
+                        name: 'Pablo Gomez Sanchez',
+                        country: 'Medellín, Colombia'
+                    },
+                    second: {
+                        review: 'El hotel está súper bien ubicado, el desayuno súper completo y la gente muy amable. Los chicos de la recepción todos muy amables ,unos genios , Matias en particular nos ayudo con todo ! Volvemos seguro!',
+                        name: 'Luciana Carpi',
+                        country: 'Buenos Aires, Argentina'
+                    },
+                    third: {
+                        review: 'Pasamos diez días maravillosos, recomendado bajo todo punto de vista, atención esmerada, y nuestra habitación (705) con dos ventanales y una terraza que permitía ver los amaneceres y atardeceres en todo su esplendor, vista al lago, la Cordillera y el Centro de la Ciudad.',
+                        name: 'Ernesto Oscar W',
+                        country: 'Rosario, Argentina'
+                    },
+                    four: {
+                        review: 'Hotel amplio en sus habitaciones cuadruples, con frigobar y television LED.... el desayuno es muy abundante y rico, la atencion es buena y amable y el precio es acorde...... es limpio y estan refaccionandolo para que este moderno',
+                        name: 'Lautaro Rosso',
+                        country: 'Mar de Plata, Argentina'
+                    },
+                    five: {
+                        review: 'reseña primero',
+                        name: 'nombre del cliente',
+                        country: 'país'
+                    }
+                }
+            },
+            contact: {
+                title: 'Contacta con nosotros',
+                phone: 'Teléfono',
+                placeholder: {
+                    name: 'Introduce tu nombre',
+                    email: 'Introduce tu dirección de email',
+                    subject: 'Introduce asunto',
+                    message: 'Introduce tu mensaje',
+                    button: 'Enviar tu mensaje'
+                }
+            },
+        },    
+     
+        rooms: {
+            banner: 'nuestras habitaciones',
+            moreOptions: 'más opciones',
+            room: {
+                economy: {
+                    title: 'Habitación Económica',
+                    description: 'Nuestra economy room cuenta con todo lo necesario para que puedas disfrutar a gusto en nuestro hotel. Descansa en nuestras comodísimas camas, seguro querrás dormir 5 minutos más!'
+
+                },
+                standard: {
+                    title: 'Habitación Standard',
+                    description: 'Habitaciones impregnadas de los tres valores más característicos del hotel: tradición, cercanía y compromiso, que hacen que alojarse en una de estas habitaciones suponga sentir la calidez del hogar, pero fuera de casa. '
+                },
+                doble: {
+                    economy: 'Doble Económica ',
+                    standard: 'Doble Estándar',
+                    description: 'Una opción tranquila para parejas que vienen a seguir enamorandose en nuestro destino o amigos que quieren disfrutar de Bariloche. En nuestras habitaciones dobles, podras descansar del movimiento de la ciudad, disfrutar de un sueño reparador que los prepararán para las aventuras que los esperan al día siguiente.'
+                },
+                triple: {
+                    economy: 'Triple Económica ',
+                    standard: 'Triple Estándar',
+                    description: '¿Quién dijo que tres son multitud? En Grand Hotel Bariloche nos gustan los números impares y por eso ofrecemos una habitación para tres personas con vistas increibles. Sea para tus amigos, to tu pequeña familia, este es tu hogar en Bariloche. '
+                },
+                cuadruple: {
+                    economy: 'Cuádruple Económica ',
+                    standard: 'Cuádruple Estándar',
+                    description: 'Todos los miembros de la familia tienen derecho a descansar bien, y los amigos.. tambien! En esta habitación con cómodos espacios podrás encontrar tu lugar de relax. '
+                }
+            },
+            services: {
+                wifi: 'Wifi Gratis',
+                air: 'Aire acondicionado'
+            }   
+        },
+        location: {
+            banner: 'ubicación',
+            map: 'Encuéntranos',
+            attractions: 'Lugares de interés',
             cards: {
                 first: {
-                    review: 'Estuvimos 3 noches alojados en pareja la pasamos muy bien. La atención del personal del comedor excelente. Habitaciones bien calefaccionadas, la y desayuno muy bueno y variado. Lo recomiendo en un 100%',
-                    name: 'Pablo Gomez Sanchez',
-                    country: 'Medellín, Colombia'
+                    restaurant: 'restaurant',
+                    text: 'texto restaurante'
                 },
                 second: {
-                    review: 'El hotel está súper bien ubicado, el desayuno súper completo y la gente muy amable. Los chicos de la recepción todos muy amables ,unos genios , Matias en particular nos ayudo con todo ! Volvemos seguro!',
-                    name: 'Luciana Carpi',
-                    country: 'Buenos Aires, Argentina'
+                    night: 'night life',
+                    text: 'casino y pubs'
                 },
                 third: {
-                    review: 'Pasamos diez días maravillosos, recomendado bajo todo punto de vista, atención esmerada, y nuestra habitación (705) con dos ventanales y una terraza que permitía ver los amaneceres y atardeceres en todo su esplendor, vista al lago, la Cordillera y el Centro de la Ciudad.',
-                    name: 'Ernesto Oscar W',
-                    country: 'Rosario, Argentina'
+                    art: 'art & culture',
+                    text: 'museo'
                 },
                 four: {
-                    review: 'Hotel amplio en sus habitaciones cuadruples, con frigobar y television LED.... el desayuno es muy abundante y rico, la atencion es buena y amable y el precio es acorde...... es limpio y estan refaccionandolo para que este moderno',
-                    name: 'Lautaro Rosso',
-                    country: 'Mar de Plata, Argentina'
+                    gastonomy: 'gastronomy',
+                    text: 'chocolateria'
                 },
-                five: {
-                    review: 'reseña primero',
-                    name: 'nombre del cliente',
-                    country: 'país'
+                fifth: {
+                    adv: 'adventure',
+                    text: 'excursiones'
+                },
+                six: {
+                    center: 'Center bariloche',
+                    text: 'centro civico'
                 }
-            }
+            },
         },
-        contact: {
-            title: 'Contacta con nosotros',
-            phone: 'Teléfono',
-            placeholder: {
-                name: 'Introduce tu nombre',
-                email: 'Introduce tu dirección de email',
-                subject: 'Introduce asunto',
-                message: 'Introduce tu mensaje',
-                button: 'Enviar tu mensaje'
-            }
+        footer: {
+            titulo: 'Secciones',
+            about: 'Sobre nosotros',
+            video: 'Video',
+            gallery: 'Galería',
+            location: 'Mapa',
+            attractions: 'Lugares de interés',
+            room1: 'Standar room',
+            room2: 'Economy room',
+            contact: 'Contacto'
         },
-     },
-     rooms: {
-        banner: 'nuestras habitaciones',
-        moreOptions: 'más opciones',
-        room: {
-            economy: {
-                title: 'Habitación Económica',
-                description: 'Nuestra economy room cuenta con todo lo necesario para que puedas disfrutar a gusto en nuestro hotel. Descansa en nuestras comodísimas camas, seguro querrás dormir 5 minutos más!'
-
-            },
-            standard: {
-                title: 'Habitación Standard',
-                description: 'Habitaciones impregnadas de los tres valores más característicos del hotel: tradición, cercanía y compromiso, que hacen que alojarse en una de estas habitaciones suponga sentir la calidez del hogar, pero fuera de casa. '
-            },
-            doble: {
-                economy: 'Doble Económica ',
-                standard: 'Doble Estándar',
-                description: 'Una opción tranquila para parejas que vienen a seguir enamorandose en nuestro destino o amigos que quieren disfrutar de Bariloche. En nuestras habitaciones dobles, podras descansar del movimiento de la ciudad, disfrutar de un sueño reparador que los prepararán para las aventuras que los esperan al día siguiente.'
-            },
-            triple: {
-                economy: 'Triple Económica ',
-                standard: 'Triple Estándar',
-                description: '¿Quién dijo que tres son multitud? En Grand Hotel Bariloche nos gustan los números impares y por eso ofrecemos una habitación para tres personas con vistas increibles. Sea para tus amigos, to tu pequeña familia, este es tu hogar en Bariloche. '
-            },
-            cuadruple: {
-                economy: 'Cuádruple Económica ',
-                standard: 'Cuádruple Estándar',
-                description: 'Todos los miembros de la familia tienen derecho a descansar bien, y los amigos.. tambien! En esta habitación con cómodos espacios podrás encontrar tu lugar de relax. '
-            }
+        gallery : {
+            title: 'Galería',
+            bannerTitle: 'Galería de imágenes'
         },
-        services: {
-            wifi: 'Wifi Gratis',
-            air: 'Aire acondicionado'
-        }   
-     },
-     location: {
-         banner: 'ubicación',
-         map: 'Encuéntranos',
-         attractions: 'Lugares de interés',
-         cards: {
-            first: {
-                restaurant: 'restaurant',
-                text: 'texto restaurante'
-            },
-            second: {
-                night: 'night life',
-                text: 'casino y pubs'
-            },
-            third: {
-                art: 'art & culture',
-                text: 'museo'
-            },
-            four: {
-                gastonomy: 'gastronomy',
-                text: 'chocolateria'
-            },
-            fifth: {
-                adv: 'adventure',
-                text: 'excursiones'
-            },
-            six: {
-                center: 'Center bariloche',
-                text: 'centro civico'
-            }
-
-     },
-     footer: {
-         titulo: 'Secciones',
-         about: 'Sobre nosotros',
-         video: 'Video',
-         gallery: 'Galería',
-         location: 'Mapa',
-         attractions: 'Lugares de interés',
-         room1: 'Standar room',
-         room2: 'Economy room',
-         contact: 'Contacto'
-     },
-     gallery : {
-         title: 'Galería',
-         bannerTitle: 'Galería de imágenes'
-     },
-    }
+        
     },
 
     english: {
