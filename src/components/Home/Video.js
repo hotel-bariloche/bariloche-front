6aae9d16bdde1989f2f0ad15a5436da611ba1c85
AlundraOz <ailuozols@gmail.com>
2020-06-30 10:57:15 +0200

    package json build

--- src/components/Home/Video.js
+++ src/components/Home/Video.js
@@ -5,7 +5,7 @@ const Video = () => {
         <section className="video-area">
         <div className="container">
           <div className="row justify-content-center align-items-center flex-column text-center">
-            <a id="play-home-video" className="video-play-button" href="https://www.youtube.com/watch?v=vParh5wE-tM">
+            <a id="play-home-video" className="video-play-button" href="https://www.youtube.com/watch?v=WU_dWnyGZSo">
               <span></span>
             </a>
             <h3>Seaplace</h3>
