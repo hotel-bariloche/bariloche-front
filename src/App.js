b7ecafb92af8a48be63664c670ddc2c345f1042c
AlundraOz <ailuozols@gmail.com>
2020-07-03 11:51:26 +0200

    basic gallery

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Rooms from './components/Rooms/Rooms';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/footer';
 import AppContext from './Context/Provider';
-import Gallery from './components/Gallery/gallery';
+import Galeria from './components/Galeria/Galeria';
 
 
 class App extends React.Component {
@@ -27,8 +27,8 @@ class App extends React.Component {
             <Route path="/contacto" component= {Contact}>
               <Contact/>
             </Route>
-            <Route path="/gallery" component= {Gallery}>
-              <Gallery/>
+            <Route path="/gallery" component= {Galeria}>
+              <Galeria/>
             </Route>
           </Switch>
         <Footer/>
