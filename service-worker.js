ba666402e7225ff1f38af5a9a3994779c91e9018
Juan Pablo Monaco <joh1989@gmail.com>
2020-07-16 11:31:35 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b479f3b62e54a0ad54c0b6c267483bb.js"
+  "/precache-manifest.a3734aa4e16743dfc956f674cfecebbf.js"
 );
 
 self.addEventListener('message', (event) => {
