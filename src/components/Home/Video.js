02d3c2230f59c668a959911ecdb0ed1e15cda1db
AlundraOz <ailuozols@gmail.com>
2020-07-29 17:32:45 +0200

    trying styles in modal

--- src/components/Home/Video.js
+++ src/components/Home/Video.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
+
+
 
 const Video = () => {
   const [show, setShow] = useState(false);
