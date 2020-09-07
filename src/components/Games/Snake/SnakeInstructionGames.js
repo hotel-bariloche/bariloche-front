1da8cf51b9e2eee9b2b19459cfd9fb574a8b58a2
Sandra <alecsandra.serediuc@gmail.com>
2020-09-07 14:17:39 +0200

    Snake instructions from interogante modal styling

--- src/components/Games/Snake/SnakeInstructionGames.js
+++ src/components/Games/Snake/SnakeInstructionGames.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {
-    Button, Modal, ModalBody, ModalFooter
+    Button, Modal, ModalBody, ModalFooter, ModalHeader
 } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 import './SnakeInstructionGames.scss'
+import { Context } from '../../../Context/Provider';
+import ToolTipConditions from '../ToolTipConditions';
 
 export class SnakeInstructionGames extends React.Component {
+    static contextType = Context
     constructor(props) {
         super(props);
         this.state = {
@@ -25,19 +28,45 @@ export class SnakeInstructionGames extends React.Component {
            faQuestionCircle
        }
        />
+       let context = () => {            
+        const context = this.context;
+        return context;
+    }
 
+    let language = () => {            
+        const language = context().state.language;
+        return language;
+    } 
       return (
         < div className = "instructionGamesSnake" >
             {/* INSTRUCTIONS */}
                 <span style={{ color: "#29ABE2", fontSize: "2em" }} onClick={this.toggle}>{instructions}</span>
-                <Modal isOpen={this.state.modal} toggle={this.toggle} style={{ marginTop:"10%"}}>
+                            <Modal isOpen={this.state.modal} toggle={this.toggle} style={{ marginTop:"10%"}}>
+                <div className='bodyModalSnake'>
+                    <ModalHeader toggle={this.toggle}>
+                        <div className="snakeModalTitle">
+                            {context().state.texts[language()].memoryGame.popupTitle}
+                        </div>
+                    </ModalHeader>
                     <ModalBody>
-                        <div>{this.props.instructionText}</div>
+                        <div className='snakeDespription'><p>                
+                            {context().state.texts[language()].snake.popup_description} 
+                        </p></div>
+                        <ul className='snakeListDiscount'>
+                            <li>{context().state.texts[language()].snake.instruction1}</li>
+                            <li>{context().state.texts[language()].snake.instruction2}</li>
+                            <li>{context().state.texts[language()].snake.instruction3}</li>
+                            <li>{context().state.texts[language()].snake.instruction4}</li>
+                        </ul>
                     </ModalBody>
                     <ModalFooter>
-                        <Button color="primary" onClick={this.toggle}>Cerrar</Button>
+                        <div>
+                            <ToolTipConditions/>
+                        </div>
+                        {/* <Button color="secondary" onClick={this.toggle}>{context().state.texts[language()].memoryGame.close_button}</Button> */}
                     </ModalFooter>
-                </Modal>
+                 </div>
+            </Modal>
         </div>
     )}
 }
