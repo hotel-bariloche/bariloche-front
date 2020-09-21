054193fbd921d3d8cebeb38a734430456a61feac
Sandra <alecsandra.serediuc@gmail.com>
2020-09-21 10:50:40 +0200

    Added the timer for the questions

--- src/components/Games/Trivia/Card/CardModal.js
+++ src/components/Games/Trivia/Card/CardModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, ModalBody } from 'reactstrap';
 import CardQuestion from '../CardQuestions/CardQuestion';
 
@@ -7,18 +7,24 @@ import CardQuestion from '../CardQuestions/CardQuestion';
 const CardModal = (props) => {
 
   const [modal, setModal] = useState(true);
-
-
+  const [timer, setTimer] = useState(10);
 
   const handleClose = (props) => {
-      setModal(false);
-  }
+    setModal(false);
+}
+useEffect(() => {
+    if (timer > 0) {
+      setTimeout(() => setTimer(timer - 1), 1000);
+    } else {
+      setModal(handleClose());
+    }
+  }, [timer]);
 
-  
     return(
         <div>
           <Modal backdrop='static' isOpen={modal} toggle={handleClose} style={{ marginTop:"10%"}}>
             <ModalBody>
+            <div>{timer}</div>
                 <CardQuestion 
                   question={props.question}
                   correct={props.correct} 
